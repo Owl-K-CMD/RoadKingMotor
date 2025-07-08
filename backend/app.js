c97091132631efc687fa359d6a1a16db45d11020
unknown <kumutimam@gmail.com>
2025-07-08 17:40:59 +0200

    Initial commit

--- backend/app.js
+++ backend/app.js
@@ -1,83 +1,8 @@
-/*
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const path = require('path'); 
-
-const config = require('./utils/config');
-const logger = require('./utils/logger');
-const middleware = require('./utils/middleware');
-
-const motorsRouter = require('./controller/Motor');
-const messageRouter = require('./controller/Message');
-const usersRouter = require('./controller/User');
-const cartRouter = require('./controller/cartRoute');
-const commentRouter = require('./controller/Comment');
-
-
-const app = express();
-
-logger.info('connecting to', config.MONGODB_URI);
-
-mongoose
-  .connect(config.MONGODB_URI)
-  .then(() => {
-    logger.info('connected to MongoDB');
-  })
-  .catch((error) => {
-    logger.error('error connection to MongoDB:', error.message);
-  });
-
-  const allowedOrigins = [
-  'https://roadkingmotor-pkx5.onrender.com',
-  'http://localhost:5174',
-  'http://localhost:5173',
-  ]
-
-  const corsOptions = {
-  origin: (origin, callback) => {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true,
-  }
-
-
-
-app.use(cors(corsOptions));
-app.use(express.json());
-app.use(middleware.requestLogger);
-
-app.use('/api/motors', motorsRouter);
-app.use('/api/messages', messageRouter);
-app.use('/api/user', usersRouter);
-app.use('/api/cart', cartRouter);
-app.use('/api/comments', commentRouter);
-
-app.use(express.static(path.join(__dirname, 'dist')));
-
-app.get('*', (request, response) => {
-  response.sendFile(path.join(__dirname, 'dist', 'index.html'));
-});
-
-
-
-
-app.use(middleware.unknownEndpoint);
-app.use(middleware.errorHandler);
-
-module.exports = app
-*/
-
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const path = require('path'); 
+const rateLimit = require('express-rate-limit');
 
 const config = require('./utils/config');
 const logger = require('./utils/logger');
@@ -97,6 +22,12 @@ mongoose.connect(config.MONGODB_URI)
   .then(() => logger.info('connected to MongoDB'))
   .catch((error) => logger.error('MongoDB connection error:', error.message));
 
+// Rate limiting middleware
+const pingLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
+  message: 'Too many requests, please try again later.',
+});
 // CORS config
 const allowedOrigins = [
   'https://roadkingmotor-pkx5.onrender.com',
@@ -124,14 +55,11 @@ app.use('/api/messages', messageRouter);
 app.use('/api/user', usersRouter);
 app.use('/api/cart', cartRouter);
 app.use('/api/comments', commentRouter);
-/*
-// Serve frontend build
-app.use(express.static(path.join(__dirname, 'dist')));
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+
+app.get('/api/ping', pingLimiter, (request, response) => {
+  response.status(200).send('pong');
 });
 
-*/
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
 
