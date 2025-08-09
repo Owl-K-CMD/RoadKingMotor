b6c12f5ee6c63dde23507c4ee2a439ff22e7e8e6
unknown <kumutimam@gmail.com>
2025-08-10 00:25:34 +0200

    Initial commit

--- frontend/src/axios/commentAxios.js
+++ frontend/src/axios/commentAxios.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { refreshAuthToken } from '../refreshToken'
 
 //const commentURL = '/api/comments'
-const commentURL = `${import.meta.env.VITE_API_BASE_URL || ''}http://localhost:5000/api/comments`
+const commentURL = `${import.meta.env.VITE_API_BASE_URL || ''}/api/comments`
 let isRefreshing = false;
 let failedQueue = [];
 let onLogoutCallback = null;
